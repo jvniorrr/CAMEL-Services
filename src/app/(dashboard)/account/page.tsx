34d70edfd4dea2e0b9797afd9c1684d82db464db
imrenmore <imrenkmore@gmail.com>
2024-02-11 21:29:43 -0800

    SO-64: avatar component onto page with role and username printed

---
+++ src/app/(dashboard)/account/page.tsx
@@ -0,0 +1,29 @@
+import { AccountInputField } from '@/components/Account/AccountInputField/AccountInputField';
+import { getOrganizationMemberRole, getUserInformation } from '@/lib/actions';
+import { IUsers_table } from '@/types/database.interface';
+import { cookies } from 'next/headers';
+
+const AccountPage = async () => {
+	// references backend username for First Name, Last Name initials of user
+	const userInfo = await getUserInformation();
+	const username = userInfo?.name || '';
+
+	// check if the user has a valid organization stored in their cookies
+	const cookieStore = cookies();
+	const org = cookieStore.get('org')?.value as string;
+
+	// references backend for role of user
+	const roleResponse = await getOrganizationMemberRole(org);
+	const role: string = roleResponse?.role || '';
+
+	return (
+		<>
+			<AccountInputField
+				userrole={role}
+				user={userInfo as IUsers_table}
+			/>
+		</>
+	);
+};
+
+export default AccountPage;
