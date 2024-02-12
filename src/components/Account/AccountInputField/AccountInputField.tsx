8a9cb0976f3a759319522338526f97599659dca2
imrenmore <imrenkmore@gmail.com>
2024-02-11 21:33:20 -0800

    SO-61- User Profile Web Page View

--- src/components/Account/AccountInputField/AccountInputField.tsx
+++ src/components/Account/AccountInputField/AccountInputField.tsx
@@ -1,69 +1,112 @@
-import React from 'react';
 import './AccountInputField.css';
-import Avatar from '../Avatar/Avatar';
+import Avatar from '@/components/Account/Avatar/Avatar';
 import Buttons from '@/components/SharedComponents/Buttons';
+import { IUsers_table } from '@/types/database.interface';
+import InputField from '@/components/Account/Inputfield/Inputfield';
+import '@/components/Account/Inputfield/Inputfield';
 
 interface UserProfileProps {
-	name: string;
-	role: string;
-}
-interface AccountInputFieldProps {
-	label: string;
-	placeholder: string;
-	type: 'text' | 'email' | 'password';
+	user: IUsers_table;
+	userrole: string;
 }
 
-const example: AccountInputFieldProps[] = [
-	{ label: 'First Name', placeholder: 'First Name', type: 'text' },
-	{ label: 'Last Name', placeholder: 'Last Name', type: 'text' },
-	{ label: 'Email', placeholder: 'Email', type: 'email' },
-	{ label: 'Set New Password', placeholder: 'Password', type: 'password' },
-	{
-		label: 'Confirm Password',
-		placeholder: 'Confirm Password',
-		type: 'password',
-	},
-];
+const AccountInputField = ({ user, userrole }: UserProfileProps) => {
+	const { name, email, username } = user;
 
-const AccountInputField = ({ name, role }: UserProfileProps) => {
+	// const handleSubmit = async (event: React.FormEvent) => {
+	// 	const { data, error } = await supabase.auth.updateUser({email: 'new@email.com'});
+	// };
+
 	return (
-		<div>
-			<div className="accountcontainer">
-				<div className="accountinfo">
-					<Avatar name={name} />
-					<div className="user-profile">
-						<div className="name">{name}</div>
-						<div className="role">{role}</div>
-					</div>
-				</div>
-				{example.map((inpt, index) => (
-					<div
-						key={inpt.label}
-						className="infocolumns"
+		<>
+			<div className="w-full">
+				<div className="flex items-center ">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						className="h-10 w-10 mx-2 text-primary-green-600"
 					>
-						<label
-							htmlFor={inpt.label}
-							className="labelsformat"
-						>
-							{inpt.label}
-						</label>
-						<input
-							type={inpt.type}
-							id={inpt.label}
-							className="inputf"
-							placeholder={inpt.placeholder}
-							required
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
 						/>
+					</svg>
+					<div className="accounttitle">Account</div>
+				</div>
+				<div className="accountcontainer">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						className="w-10 h-10 text-primary-green-600"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
+						/>
+					</svg>
+
+					<div className="accountinfo">
+						<Avatar name={name} />
+						<div className="user-profile">
+							<div className="name">{name}</div>
+							<div className="role">{userrole}</div>
+						</div>
 					</div>
-				))}
-				<Buttons
-					variant="primary"
-					content="Save"
-					size="medium"
-					className="mx-2 my-2"
-				/>
+
+					{/* firstname */}
+					<InputField
+						label="First Name"
+						placeholder={name.split(' ')[0]}
+						type="text"
+						//onSubmit={handleSubmit('firstname')}
+					/>
+					{/* last name */}
+					<InputField
+						label="Last Name"
+						placeholder={name.split(' ')[1]}
+						type="text"
+						//onSubmit={handleSubmit('lastname')}
+					/>
+
+					{/* username */}
+					<InputField
+						label="Username"
+						placeholder={username}
+						type="text"
+						//onSubmit={handleSubmit('username')}
+					/>
+
+					{/* email */}
+					<InputField
+						label="Email"
+						placeholder={email}
+						type="email"
+						//onSubmit={handleSubmit('email')}
+					/>
+
+					<Buttons
+						variant="primary"
+						content="Save"
+						size="medium"
+						className="mx-2 my-2"
+					/>
+					{/* <Buttons
+						variant="primary"
+						content="Reset Password"
+						size="medium"
+						className="mx-2 my-2"
+					/> */}
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
